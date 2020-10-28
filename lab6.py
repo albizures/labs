40c2560d0c071bc2506e5d8032d1690c41a53749
Jose Albizures <jose@albizures.com>
2020-10-28 13:38:40 +0300

    improve lab 6

--- lab6.py
+++ lab6.py
@@ -3,42 +3,63 @@
 numbers = []
 
 numbers = list(map(float, input("Initialize the list: ").split()))
+space = "|\t\t\t\t\t|"
 
 while True:
-    print("|== Menu =========|")
-    print("|  1. Add a number\t|")
-    print("|  2. Remove a number\t|")
-    print("|  3. Clear list\t|")
-    print("|  4. Find the max and Min values\t| ")
-    print("|  7. Exit \t|")
-    print("|===============|")
-    print("| Current list: ", numbers)
-
+    print("|== Current list =======================|")
+    print(space)
     for index, number in enumerate(numbers):
-        print("\t", index + 1, ")", number)
-    print("|===============| \n")
+        print("|    {}) {} \t\t\t\t|".format(
+            index + 1, number))
+    print(space)
+    print("|== Menu ===============================|")
+    print(space)
+    print("|  1. Add a number\t\t\t|")
+    print("|  2. Remove a number\t\t\t|")
+    print("|  3. Clear list\t\t\t|")
+    print("|  4. Find the max and min values\t| ")
+    print("|  7. Exit \t\t\t\t|")
+    print(space)
+    print("|=======================================|\n")
 
-    action = int(input('enter which action: '))
+    action = int(input('> Enter which action you want to perform: '))
+    length = len(numbers)
 
     if 1 == action:
-        new = float(input("enter the new number: "))
-        position = int(input("enter its the position: "))
-        numbers.insert(position, new)
-    if 2 == action:
-        position = int(input("enter the position of the number to remove: "))
-        numbers.remove(position)
-    if 3 == action:
+        while True:
+            new = float(input("> Enter the new number: "))
+            position = int(input("> Enter the position: ")) - 1
+            if position < 0 or position > length + 1:
+                print("**The position is invalid**")
+            else:
+                numbers.insert(position, new)
+                break
+    elif 2 == action:
+        while True:
+            position = int(input("> Enter the position of the number to remove: "))
+            if position < 0 or position > length:
+                print("**The position is invalid**")
+            else:
+                numbers.remove(position)
+                break
+
+    elif 3 == action:
         numbers = []
-    if 4 == action:
+        print("The list has been cleaned")
+    elif 4 == action:
         maxi = 0
         mini = 0
-        for index, number in numbers:
+        for index, number in enumerate(numbers):
             if number > numbers[maxi]:
                 maxi = index
             if number < numbers[mini]:
                 mini = index
-        print("Max value: ", maxi)
-        print("Min value: ", maxi)
+        print("Max value: ", numbers[maxi])
+        print("Min value: ", numbers[mini])
+        input("> Enter to continue")
 
-    if 8 == action:
+    elif 7 == action:
         break
+    else:
+        print("**Invalid action**")
+        input("> Enter to continue")
