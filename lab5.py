10b524548091a23b5a70d3b11acaeaa8b531a2e3
Jose Albizures <jose@albizures.com>
2020-11-06 12:29:44 +0300

    add values

--- lab5.py
+++ lab5.py
@@ -1,21 +1,14 @@
-from math import inf
-start = float(input("start: "))
-end = float(input("end: "))
-step = float(input("step: "))
+value = float(input("Value: "))
+steps = int(input("steps: "))
 
-y = start
-
-biggestD = -inf
-smallestD = inf
-
-
-print(" ---------------------------- " * 3)
+print(" ---------------" * 3)
 print("|\tN\t|\tt\t|\ts\t|")
-print(" -----------------------------" * 3)
+print(" ---------------" * 3)
 
-while y < end:
-    y += step
-    print("|\t{}\t|".format(y))
+sum = 0
+for i in range(0, steps):
+    current = value ** i
+    sum += current * (1 if i % 2 == 0 else -1)
+    print("|\t{}\t|\t{}\t|\t{}\t|".format(i + 1, current, sum))
 
-
-print(" -----------------------------" * 3)
+print(" ---------------" * 3)
