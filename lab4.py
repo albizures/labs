32353e5e836e3206025e00451daffdf8311effc2
Jose Albizures <jose@albizures.com>
2020-11-21 12:36:01 +0300

    done

--- lab4.py
+++ lab4.py
@@ -5,57 +5,74 @@ step = float(input("step: "))
 
 y = start
 
-biggestD = -inf
-smallestD = inf
-
-ds = []
+biggestValue = -inf
+smallestValue = inf
 
 print(" ----------------------"*2)
 print("|{:^22}|{:^22}|".format("y", "d"))
 print(" ----------------------"*2)
 
+
+values = []
 while y <= end:
     d = y**5 - 7.9 * y**4 + 24.49 * y**3 - 37.074 * y**2 + 27.512 * y - 8.0042
     print("|{:^22.4f}|{:^22.4f}|".format(y, d))
-    ds.append(d)
-    if d > biggestD:
-        biggestD = d
-    if d < smallestD:
-        smallestD = d
+    values.append({
+        'y': y,
+        'd': d,
+    })
+    if d > biggestValue:
+        biggestValue = d
+    if d < smallestValue:
+        smallestValue = d
     y += step
 
 print(" ----------------------"*2, '\n')
 
-dRange = abs(biggestD - smallestD)
+ticks = int(input("Amout of ticks: "))
+
+total = abs(smallestValue - biggestValue)
 steps = 80
-markEach = steps // 4
-dStep = dRange / steps
+pointRange = total / steps
+inBetweenTicks = steps // ticks
+
+print(inBetweenTicks)
+rangeStep = total / steps
+
+points = []
 numberHeader = '{:^7}'.format("")
 lineHeader = '{:^11}'.format("")
-values = ''
-points = []
+rows = ""
 
-for index in range(0, steps + 1):
-    num = smallestD + dStep * index
+for index in range(0, steps + 2):
+    num = smallestValue + pointRange * index
     points.append(num)
-    if index % markEach == 0:
-        sNum = '{:^10.1f}'.format(num)
-        numberHeader += sNum + (' ' * (markEach - len(sNum)))
+
+    if index % inBetweenTicks == 0 and index < steps:
+        sNum = '{:^10.2f}'.format(num)
+        numberHeader += sNum + (' ' * (inBetweenTicks - len(sNum)))
         lineHeader += '|'
     else:
         lineHeader += '-'
+lineHeader += '> y'
 
-
-for i, d in enumerate(ds):
-    for index, num in enumerate(points):
-        nextNum = smallestD + dStep * (index + 1)
-        if d >= num and d <= nextNum:
-            padding = ' ' * index
-            values = "{}{:>10.3f} |{}*\n".format(
-                values, start + i * step, padding)
-            break
+for value in values:
+    y = value['y']
+    d = value['d']
+    rows = values = "{}{:>10.3f} ".format(rows, y)
+    for index, point in enumerate(points):
+        if len(points) - 1 != index:
+            nextPoint = points[index + 1]
+            if point <= d < nextPoint:
+                rows += '*'
+            elif point <= 0 < nextPoint:
+                print(point, nextPoint)
+                rows += '|'
+            else:
+                rows += ' '
+    rows += '\n'
 
 
 print(numberHeader)
-print(lineHeader + '-> y')
-print(values + '{:>12}\n{:>12}'.format("˅", "x"))
+print(lineHeader)
+print(rows)
