2b916b4d98c5d3674b43060eb1dfd676c99b014f
Jose Albizures <jose@albizures.com>
2021-05-31 15:04:16 +0300

    improve code

--- semester_2/labs/n3.py
+++ semester_2/labs/n3.py
@@ -17,19 +17,20 @@ def d2f(x):
 def steff_by_steps(f, a, b, eps, steps):
     step = (b - a) / steps
     x1 = a
-    while (x1 <= b):
+
+    results = []
+    while (x1 < b):
         x2 = x1 + step
         f1 = f(x1)
         f2 = f(x2)
         if f1 * f2 <= 0:
             result = steff(f, x1, x2, eps)
-            if result == None:
-                break
-
-            x, fx, counter = result
-            return str(x), f"({x1};{x2})", str(x1), str(fx), str(counter), '',
+            x, fx, iteration, error_code = result
+            results.append((len(results) + 1, f"({x1};{x2})", x, str(fx), str(iteration), error_code))
+        else:
+            results.append((len(results) + 1, f"({x1};{x2})", '', '', '', 'Not Found'))
         x1 = x2
-    return '', "", "", "", "", 'Not found',
+    return results
 
 
 def steff(f, a, b, eps):
@@ -40,12 +41,12 @@ def steff(f, a, b, eps):
         fx = f(x)
 
         if(abs(fx) < eps):
-            return x, fx, counter
+            return x, fx, counter, ''
 
         x -= (fx**2) / (f(x + fx) - fx)
 
         if x >= b:
-            return None
+            return '', '', '', 'Not found',
 
 
 class App(tk.Tk):
@@ -53,17 +54,30 @@ class App(tk.Tk):
         super().__init__()
 
         self.bind('<Escape>', self.quit)
-
+        self.columns = []
         self.init_ui()
         self.mainloop()
 
-    def create_result_col(self, text, column):
-        value = tk.StringVar()
-        tk.Label(self.table_frame, borderwidth=2, relief=tk.RIDGE, text=text).grid(row=0, column=column, sticky=tk.NSEW)
-        label = tk.Label(self.table_frame, textvariable=value, borderwidth=2, relief=tk.RIDGE)
-        label.grid(row=1, column=column, sticky=tk.NSEW)
+    def create_headers(self):
+        tk.Label(self.table_frame, borderwidth=2, relief=tk.RIDGE, text=f"{'Root Number':^35}").grid(row=0, column=0, sticky=tk.NSEW)
+        tk.Label(self.table_frame, borderwidth=2, relief=tk.RIDGE, text=f"{'[xi; xi+1]':^20}").grid(row=0, column=1, sticky=tk.NSEW)
+        tk.Label(self.table_frame, borderwidth=2, relief=tk.RIDGE, text=f"{'x':^40}").grid(row=0, column=2, sticky=tk.NSEW)
+        tk.Label(self.table_frame, borderwidth=2, relief=tk.RIDGE, text=f"{'f(x)':^40}").grid(row=0, column=3, sticky=tk.NSEW)
+        tk.Label(self.table_frame, borderwidth=2, relief=tk.RIDGE, text="Number of iterations").grid(row=0, column=4, sticky=tk.NSEW)
+        tk.Label(self.table_frame, borderwidth=2, relief=tk.RIDGE, text="Error code").grid(row=0, column=5, sticky=tk.NSEW)
 
-        return value
+    def clean_rows(self):
+        for column in self.columns:
+            for row in column:
+                row.grid_forget()
+
+    def show_result(self, results):
+        self.clean_rows()
+
+        for column in range(6):
+            for row, result in enumerate(results):
+                label = tk.Label(self.table_frame, text=result[column], borderwidth=2, relief=tk.RIDGE)
+                label.grid(row=row+1, column=column, sticky=tk.NSEW)
 
     def init_ui(self):
         self.start_point = tk.StringVar(value="-4")
@@ -94,12 +108,7 @@ class App(tk.Tk):
         self.table_frame = tk.Frame(self)
         self.table_frame.grid(row=1, column=0, padx=20, pady=20)
 
-        self.root_number = self.create_result_col(text=f"{'Root Number':^35}", column=0)
-        self.section = self.create_result_col(text=f"{'[xi; xi+1]':^20}", column=1)
-        self.x = self.create_result_col(text=f"{'x':^40}", column=2)
-        self.f_x = self.create_result_col(text=f"{'f(x)':^40}", column=3)
-        self.iterations = self.create_result_col(text="Number of iterations", column=4)
-        self.error = self.create_result_col(text="Error code", column=5)
+        self.create_headers()
 
     def calculate(self):
         a = float(self.start_point.get())
@@ -111,13 +120,8 @@ class App(tk.Tk):
             # swap
             a, b = b, a
 
-        root_number, section, x, fx, iteractions, error = steff_by_steps(f, a, b, eps, steps)
-        self.root_number.set(root_number)
-        self.section.set(section)
-        self.x.set(x)
-        self.f_x.set(fx)
-        self.iterations.set(iteractions)
-        self.error.set(error)
+        results = steff_by_steps(f, a, b, eps, steps)
+        self.show_result(results)
 
     def quit(self, *args):
         self.destroy()
